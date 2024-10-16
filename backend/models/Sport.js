67c2b773877289533f9d228216ac7961ac0d191e
MOHIT KUMAR <mohit7282kumar@gmail.com>
2024-10-16 21:05:28 +0530

    server file updation

--- backend/models/Sport.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const sportSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-});
-
-module.exports = mongoose.model("Sport", sportSchema);
--- backend/routes/centerRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createCentre, getCentres } = require("../controllers/centerController");
-const { authenticate } = require("../middleware/authMiddleware");
-const router = express.Router();
-
-router.post("/", authenticate, createCentre);
-router.get("/", authenticate, getCentres);
-
-module.exports = router;
