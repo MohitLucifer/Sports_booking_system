61d1f1cc39c27053d24d7005ad7201e8383928bf
MOHIT KUMAR <mohit7282kumar@gmail.com>
2024-10-16 23:36:47 +0530

    cors error fix

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import adminRoute from './routes/adminRoutes.js';
-import userRoute from './routes/userRoutes.js';
-import connectdb from './dbconfig/connectdb.js';
+import express from "express";
+import cors from "cors";
+import adminRoute from "./routes/adminRoutes.js";
+import userRoute from "./routes/userRoutes.js";
+import connectdb from "./dbconfig/connectdb.js";
 
 const app = express();
 
@@ -10,20 +10,33 @@ const app = express();
 connectdb();
 
 //middlewares
-app.use(cors());
+// app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://admingametheory.netlify.app/"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(express.json());
 
 //routes
-app.use('/api/v1/admin', adminRoute);
-app.use('/api/v1/user', userRoute);
+app.use("/api/v1/admin", adminRoute);
+app.use("/api/v1/user", userRoute);
 
 //img uploads
-app.use('uploads/', express.static('uploads'));
+app.use("uploads/", express.static("uploads"));
 
 //port
 const PORT = process.env.PORT || 8080;
 
-//listen 
+//listen
 app.listen(PORT, () => {
-    console.log(`Server started on port: ${PORT}`);
+  console.log(`Server started on port: ${PORT}`);
 });
