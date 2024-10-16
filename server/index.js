1c0eb2c3fa1786b60d03cbe2c5bdc60ca3d00dc7
MOHIT KUMAR <mohit7282kumar@gmail.com>
2024-10-16 23:40:46 +0530

    cors updated 2

--- server/index.js
+++ server/index.js
@@ -11,18 +11,19 @@ connectdb();
 
 //middlewares
 // app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://admingametheory.netlify.app/"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(cors({ origin: "https://admingametheory.netlify.app" }));
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://admingametheory.netlify.app/"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, OPTIONS"
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 
 app.use(express.json());
 
